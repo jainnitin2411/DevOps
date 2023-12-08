34da73ab1a88b8dc71a30bd2bf9836424931c24a
NJ <jainnitin2411@users.noreply.github.com>
2023-12-09 00:49:39 +0530

    Update Program.cs

--- JenkinsConsoleApp/JenkinsConsoleApp/Program.cs
+++ JenkinsConsoleApp/JenkinsConsoleApp/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-// Print the Current Date
+// Print the Current Date.
 Console.WriteLine(DateTime.Now);
